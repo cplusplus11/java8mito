44ba72318e275d41849c7dfa63e873702db9c4db
Luis <luisalberto.huasacca@gmail.com>
2018-05-16 17:58:01 -0500

    Se modifico a Luis en Anotaciones app

--- src/main/java/com/mitocode/anotaciones/App.java
+++ src/main/java/com/mitocode/anotaciones/App.java
@@ -14,7 +14,7 @@ public class App {
 	@Repeatable(value = Lenguajes.class)
 	public @interface Lenguaje{
 		String value();
-		String nombreUsuario() default "MitoCode"; 
+		String nombreUsuario() default "MitoCode Luis"; 
 	}
 	
 	//1.7
